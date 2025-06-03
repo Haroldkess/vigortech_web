ec7aff985faebf781cb5a03ad234c33e4732ba23
Akpan Joseph <syntaxdesigners@gmail.com>
2025-06-03 05:11:19 +0100

    feat: made navbar responsive

--- src/app/Component/HomePage/NavBar.tsx
+++ src/app/Component/HomePage/NavBar.tsx
@@ -1,12 +1,15 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
+import { IoIosMenu, IoIosClose } from "react-icons/io";
 
 export default function NavBar() {
+    const [showMenu,setMenu] = useState(false)
   return (
-    <nav className="flex justify-center items-center w-full mt-10 ">
-      <section className="  bg-transparent w-[85vw] text-white px-8 py-4 border shadow-2xl border-[#868686D9] rounded-2xl">
+    <nav className="flex flex-col justify-center items-center w-full mt-10  gap-y-2">
+      <section className="bg-transparent lg:w-[85vw] md:w-[90vw] w-[95vw] text-white px-8 py-4 border shadow-2xl border-[#868686D9] rounded-2xl">
         <section className="flex justify-between items-center">
           <section>
             <Image
@@ -14,24 +17,24 @@ export default function NavBar() {
               alt="logo"
               width={200}
               height={200}
-              className="w-[150px] "
+              className="lg:w-[150px] w-[100px] "
             />
           </section>
 
-          <section className="flex items-center gap-x-20">
+          <section className="flex items-center gap-x-20 ">
             <section>
-              <ul className="flex gap-4 items-center">
+              <ul className="lg:flex gap-x-6 items-center hidden">
                 <li>
                   <Link href="/">Home</Link>
                 </li>
                 <li>
-                  <section className="flex gap-2 items-center">
+                  <section className="flex gap-1 items-center">
                     Company <FaAngleDown />
                   </section>
                 </li>
                 <li>
                   <Link href="/contact">
-                    <section className="flex gap-2 items-center">
+                    <section className="flex gap-1 items-center">
                       Products <FaAngleDown />
                     </section>
                   </Link>
@@ -45,14 +48,67 @@ export default function NavBar() {
               </ul>
             </section>
 
-            <section>
-              <button className="bg-[#48B96B] px-8 py-2 text-[#171615] rounded-md">
+            <section className="lg:flex hidden ">
+              <button className="bg-[#48B96B] px-8 py-2 font-semibold  text-[#171615] rounded-md">
                 Explore our products
               </button>
+            </section>
+
+            {/* mobile button*/}
+            <section className="lg:hidden flex">
+              {!showMenu ? (
+                <IoIosMenu
+                  className="text-[25px]"
+                  onClick={() => setMenu(!showMenu)}
+                />
+              ) : (
+                <IoIosClose
+                  className="text-[30px]"
+                  onClick={() => setMenu(!showMenu)}
+                />
+              )}
             </section>
           </section>
         </section>
       </section>
+
+      {/* mobile menu */}
+
+      {showMenu && (
+        <section className=" md:w-[90vw] w-[95vw]  text-white/65 px-10 py-8 border shadow-2xl border-[#868686D9] rounded-2xl">
+          <section>
+            <ul className="flex flex-col gap-y-10 text-xl ">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <section className="flex gap-2 items-center justify-between w-full">
+                  Company <FaAngleDown />
+                </section>
+              </li>
+              <li>
+                <Link href="/contact">
+                  <section className="flex gap-2 items-center justify-between w-full">
+                    Products <FaAngleDown />
+                  </section>
+                </Link>
+              </li>
+              <li>
+                <Link href="/courses">Courses</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </section>
+
+          <section className=" mt-10 w-full ">
+            <button className="bg-[#48B96B] px-8 py-2 w-full font-semibold text-xl text-[#171615] rounded-md">
+              Explore our products
+            </button>
+          </section>
+        </section>
+      )}
     </nav>
   );
 }
