efc3bb88188c21615e562f9f13db64477147677c
Akpan Joseph <syntaxdesigners@gmail.com>
2025-06-19 15:09:39 +0100

    feat:mobile menu reponsive and glass bg working

--- src/app/Component/HomePage/NavBar.tsx
+++ src/app/Component/HomePage/NavBar.tsx
@@ -76,7 +76,7 @@ export default function NavBar() {
               {/* mobile menu */}
 
               {showMenu && (
-                  <section className="lg:hidden md:w-[90vw] w-[95vw]  text-white/65 px-10 py-8 border shadow-2xl border-[#868686D9] rounded-2xl">
+                  <section className="lg:hidden md:w-[90vw] w-[95vw]  text-white/65 px-10 py-8 border shadow-2xl border-[#868686D9] rounded-2xl z-50 bg-transparent bg-[rgba(23, 22, 21, 0.20)] drop-shadow-xl  backdrop-blur-sm z-50 rounded-xl text-white  px-8 py-4 border shadow-2xl border-[#868686D9]  ">
                       <section>
                           <ul className="flex flex-col gap-y-10 text-xl ">
                               <li>
