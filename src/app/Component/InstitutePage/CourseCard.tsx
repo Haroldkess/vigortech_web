095bf294c4938f717ab97859c93679f6151da874
Akpan Joseph <syntaxdesigners@gmail.com>
2025-06-23 07:53:34 +0100

    feat: Institute card

--- src/app/Component/InstitutePage/CourseCard.tsx
+++ src/app/Component/InstitutePage/CourseCard.tsx
@@ -1,21 +1,29 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function CourseCard() {
+interface Card{
+  imageUrl:string,
+  cardTitle:string,
+  description:string,
+  studentLevel:string
+}
+
+// /Institute/img1.png
+export default function CourseCard({imageUrl,cardTitle,description,studentLevel}:Card) {
   return (
     <section className="rounded-xl font-Inter border border-[#86868659] text-[#EAF5F4A6]" style={{ background: 'linear-gradient(rgba(23, 22, 21, 80), rgba(51, 51, 51, 0.20))', }}>
       <section>
-        <Image src={'/Institute/img1.png'} alt='sise image 1' width={400} height={400} className='object-cover w-full' />
+        <Image src={imageUrl} alt='sise image 1' width={1000} height={1000} className='object-cover w-full' />
       </section>
       <section className='px-4 py-6 flex flex-col gap-y-10'>
         <section className='flex flex-col gap-2'>
-          <h3 className='text-[#FFFFFF] font-medium text-[16px]'>Full-Stack Flutter and Dart Development</h3>
-          <p className='text-[12px]'>We bring it all together: clean UI, solid business logic, robust backend, and smooth deployment.</p>
+          <h3 className='text-[#FFFFFF] font-medium text-[16px]'>{cardTitle}</h3>
+          <p className='text-[12px]'>{description}</p>
 
           <section className='flex gap-4'>
             <div className='flex text-[10px] gap-2'>
               <Image src={'/Institute/school.svg'} alt='school' width={15} height={15} className='' />
-              <span>Intermediate</span>
+              <span>{studentLevel}</span>
             </div>
 
             <div className='flex text-[10px] gap-2'>
--- src/app/Component/InstitutePage/Courses.tsx
+++ src/app/Component/InstitutePage/Courses.tsx
@@ -2,16 +2,67 @@ import React from 'react'
 import CourseCard from './CourseCard'
 
 export default function Courses() {
+
+    const contentCards = [
+        {
+            id: 1,
+            image: "/Institute/img1.png",
+            title: "Full-Stack Flutter and Dart Development",
+            description:
+                "We bring it all together: clean UI, solid business logic, robust backend, and smooth deployment.",
+            level: "Intermediate",
+        },
+        {
+            id: 2,
+            image: "/Institute/img2.png",
+            title: "Introduction to Dart Programming Language",
+            description:
+                "Learn the language that powers flutter. If you want to build fast, modern applications, Dart is your starting point.",
+            level: "Beginner",
+        },
+        {
+            id: 3,
+            image: "/Institute/img3.png",
+            title: "Flutter for Beginners: Cross-Platform App Development",
+            description:
+                "Launch your first application without years of experience. In this course, we’ll Walk you through building your first app.",
+            level: "Beginner",
+        },
+        {
+            id: 4,
+            image: "/Institute/img2.png",
+            title: "Building Backend Systems with Dart",
+            description:
+                "With this course, you’ll master backend development using Dart, create fast APIs, handle data, and app end-to-end.",
+            level: "Beginner",
+        },
+        {
+            id: 4,
+            image: "/Institute/img3.png",
+            title: "Advanced Cross-Platform Development",
+            description:
+                "We cover state management, API integration, and building scalable architecture. This is that next leap",
+            level: "Intermediate",
+        },
+    ];
+
   return (
     <section className='px-20'>
-      <section className='grid grid-cols-4 gap-10'>
-              <CourseCard />
-              <CourseCard />
-              <CourseCard />
-              <CourseCard />
-              <CourseCard />
+
+              <div className="grid grid-cols-1 md:grid-cols-4 gap-2 lg:gap-8">
+                  {contentCards.map((card, index) => (
+                      <CourseCard
+                          key={index}
+                          cardTitle={card.title}
+                          description={card.description}
+                          imageUrl={card.image}
+                       studentLevel={card.level}
+                      />
+                  ))}
+              </div>
           
-      </section>
+          
+     
     </section>
   )
 }
