20bb71dd96ec6058284f4613c039af6bcc7d09a0
Akpan Joseph <syntaxdesigners@gmail.com>
2025-06-23 15:49:58 +0100

    fix: adjust styles and layout for About Us section and HeroGeneral component

--- src/app/(Home)/about-us/page.tsx
+++ src/app/(Home)/about-us/page.tsx
@@ -22,19 +22,19 @@ export default function AboutUs() {
           <SectionTitle title='About us' />
 
           <section className=' '>
-            <h1 className='text-white text-lg md:text-5xl font-bold text-center'>We’re Building Africa’s
+            <h1 className='text-white text-3xl md:text-5xl font-bold text-center'>We’re Building Africa’s
               <br />
               tomorrow, today!</h1>
 
-            <section className='relative left-[56%] md:left-[53%] top-[-1rem] '><Image src="/newsLine.svg" alt="Newsletter Icon" width={200} height={100} className='h-3 md:h-10' /></section>
+            <section className='relative left-[48%] md:left-[53%] top-[-1rem] '><Image src="/newsLine.svg" alt="Newsletter Icon" width={200} height={100} className='h-5 md:h-10' /></section>
 
-            <section className='flex flex-col items-center gap-4  justify-center w-full'>
+            <section className='flex flex-col items-center gap-4  justify-center w-[90vw] md:w-full'>
               <p className='text-[#EAF5F4A6] text-sm md:text-lg font-normal text-center mt-4'>Where purpose meets real impact.</p>
-              <section className=" flex flex-col md:flex-row gap-4 ">
+              <section className=" flex flex-col md:flex-row gap-4 w-[90vw] md:w-[60vw] md:justify-center md:items-center">
                 <button className="bg-[#48B96B] md:px-8 px-4 py-2 font-normal text-[#171615] rounded-md">
                   Explore our products
                 </button>
-                <button className="border border-[#48B96B] px-8  py-2 font-normal justify-center rounded-md flex items-center gap-2">
+                <button className="border border-[#48B96B]  px-8  py-2 font-normal justify-center rounded-md flex items-center gap-2">
                   We&apos;re Hiring <MdOutlineArrowOutward />
                 </button>
               </section>
--- src/app/Component/General/HeroGeneral.tsx
+++ src/app/Component/General/HeroGeneral.tsx
@@ -6,13 +6,13 @@ import { PropsWithChildren } from 'react'
 
 export default function HeroGeneral({ children }: PropsWithChildren) {
     return (
-        <header className=" lg:mb-[30vh] mb-40  font-inter pb-40 lg:pb-20  ">
-            <section className="relative -z-10 top-[2em] md:top-[10em] h-[70vh] md:h-[50vh] w-full ">
+        <header className=" lg:mb-[30vh] md:mb-40 mb-20  font-inter pb-20 lg:pb-20  ">
+            <section className="relative -z-10 top-[5em] md:top-[10em] h-[70vh] md:h-[50vh] w-full ">
                 <section className="flex items-center px-4 md:px-10 lg:px-20 w-full  justify-evenly">
 
                     {/* bg image 1 */}
-                    <section className='absolute top-10 left-[-30em]'>
-                        <Image src={'/bgEarth.png'} alt='sise image 1' width={1000} height={1000} className='' />
+                    <section className='absolute left-[-95vw] top-[10em] md:top-10 md:left-[-30em] '>
+                        <Image src={'/bgEarth.png'} alt='sise image 1' width={1000} height={1000} className='w-[600px]' />
                     </section>
 
 
@@ -22,8 +22,8 @@ export default function HeroGeneral({ children }: PropsWithChildren) {
                     </section>
 
                     {/* bg image 2 */}
-                    <section className='absolute top-[-10em] right-[-30em]'>
-                        <Image src={'/bgEarth.png'} alt='sise image 1' width={1000} height={1000} className='' />
+                    <section className='absolute  right-[-95vw] top-[2em] md:top-[-10em] md:right-[-30em] '>
+                        <Image src={'/bgEarth.png'} alt='sise image 1' width={1000} height={1000} className='w-[600px]' />
                     </section>
 
                 </section>
