e41f0388385bd9944e903c0e91a65f05c84345e5
Akpan Joseph <syntaxdesigners@gmail.com>
2025-06-23 16:10:26 +0100

    fix: improve layout and structure of OurStory component for better readability

--- src/app/Component/AboutUsPage/OurStory.tsx
+++ src/app/Component/AboutUsPage/OurStory.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function OurStory() {
     return (
-        <section>
-            <section className='flex  justify-between w-full py-20 px-20 relative gap-10'>
+        <section className='mb-40'>
+            <section className='flex flex-col md:flex-col  justify-between w-full py-20 px-10 md:px-20  gap-10'>
                 <section className='pt-10'>
                     <h2 className='border-s-2 px-4 border-[#48B96B] text-3xl'>Our Story</h2>
                     <section className='text-[#EAF5F4A6] text-md  font-light text-start flex flex-col gap-y-10  mt-4 max-w-xl'>
@@ -14,18 +14,18 @@ export default function OurStory() {
                     </section>
                 </section>
 
-                <section>
+                <section >
                     <Image src="/elderly_man.png" alt="Our Story Image" width={350} height={350} className="w-full h-auto object-cover mt-10" />
                 </section>
 
-                <section>
-                    <section className='absolute bottom-[-3em] left-10'>
+                <section className='relative'>
+                    <section className='absolute   md:bottom-[-3em] left-10'>
                         <Image src={'/Earth.png'} alt='sise image 1' width={670} height={670} className='' />
                     </section>
-                    <section className='absolute bottom-[-4em] left-[-6em] -z-[4em] blur-2xl'>
+                    <section className='absolute hidden bottom-[-4em] left-[-6em] -z-[4em] blur-2xl'>
                         <Image src={'/bgEarth.png'} alt='sise image 1' width={700} height={700} className='' />
                     </section>
-                <section className="absolute bottom-24 left-[23em] z-50 ">
+                    <section className="absolute top-22 left-[11.8em] md:bottom-24 md:left-[23em] z-50 ">
                     <div className='glow-bicon w-4 h-4 rounded-full'> </div>
                 </section>
                 </section>
