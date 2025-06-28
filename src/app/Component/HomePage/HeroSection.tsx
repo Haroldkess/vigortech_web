f2bcd05c22259a8a95dcfb0c8b03f91edd85c453
Akpan Joseph <syntaxdesigners@gmail.com>
2025-06-28 17:09:48 +0100

    fix: improve layout and styling in HeroSection component for enhanced visual consistency and responsiveness

--- src/app/Component/HomePage/HeroSection.tsx
+++ src/app/Component/HomePage/HeroSection.tsx
@@ -11,26 +11,28 @@ export default function HeroSection() {
   return (
     <header className=" lg:mb-[30vh] mb-40  font-inter pb-40 lg:pb-40  ">
       <section className="relative z-10 top-[2em] md:top-[10em] h-[70vh] md:h-[50vh] w-full">
-
         <section className="flex items-center px-4 md:px-10 lg:px-10 w-full ">
           <section
             className="z-20 absolute flex flex-col gap-10
              w-[90vw] sm:w-[85vw] md:w-[50vw] lg:w-[60vw]
-             top-24 md:top-10 lg:top-auto lg:bottom-[-4em]">
-
+             top-24 md:top-10 lg:top-auto lg:bottom-[-4em]"
+          >
             <section className="flex flex-col gap-6">
-
-              <h1 className=" font-inter font-extrabold text-[32px] leading-[40px] tracking-[0%]
+              <h1
+                className=" font-inter font-extrabold text-[32px] leading-[40px] tracking-[0%]
               md:font-bold md:text-[57px] md:leading-[64px] md:tracking-[-0.25%]
-              lg:text-[60px] lg:leading-[72px] lg:tracking-[0.5%]" >
+              lg:text-[60px] lg:leading-[72px] lg:tracking-[0.5%]"
+              >
                 Empowering Africa with smart technology
               </h1>
 
               {/* <p className="md:w-[45vw] w-[90vw] text-[#EAF5F4A6] text-sm md:text-md"> */}
-              <p className="font-inter text-[16px] leading-[20px] tracking-[0.1%]
+              <p
+                className="font-inter text-[16px] leading-[20px] tracking-[0.1%]
               font-normal text-[#EAF5F4A6]
               md:font-medium md:bg-transparent
-              lg:font-medium md:w-[45vw] w-[90vw]">
+              lg:font-medium md:w-[45vw] w-[90vw]"
+              >
                 Innovative solutions that transform businesses through
                 cutting-edge technology, expert strategy, and seamless
                 integration.
@@ -39,15 +41,12 @@ export default function HeroSection() {
 
             <section>
               <section className=" flex flex-col md:flex-row gap-4 w-[90vw]">
-
-                <button className=" h-[48px] w-[228px] px-[20px] py-[10px] text-[16px] font-[500] flex justify-center items-center text-[#171615] rounded-md bg-[#48B96B] hover:bg-[#6CC788]">
+                <button className=" md:h-[48px] md:w-[228px] px-[20px] py-[10px] text-[16px] font-[500] flex justify-center items-center text-[#171615] rounded-md bg-[#48B96B] hover:bg-[#6CC788]">
                   Explore our products
                 </button>
-                <button className="group border border-[#48B96B] w-[185px] h-[48px] font-semibold justify-center rounded-md flex items-center gap-2 transition-colors duration-100 hover:bg-[#6CC78840]  hover:text-white">
+                <button className="group border border-[#48B96B] w-[357px] py-[12px] md:w-[185px] md:h-[48px] font-semibold justify-center rounded-md flex items-center gap-2 transition-colors duration-100 hover:bg-[#6CC78840]  hover:text-white">
                   Get in touch
-                  <MdOutlineArrowOutward
-                    className="transition-transform duration-100 group-hover:rotate-45 text-[24px]"
-                  />
+                  <MdOutlineArrowOutward className="transition-transform duration-100 group-hover:rotate-45 text-[24px]" />
                 </button>
               </section>
             </section>
@@ -79,7 +78,6 @@ export default function HeroSection() {
           {/* desktop image section */}
           <section className=" hidden lg:block">
             <section className=" flex justify-center flex-col  ">
-
               <img
                 src={"/Earth.png"}
                 alt="earth"
@@ -107,18 +105,14 @@ export default function HeroSection() {
                   <div className="layer middle"></div>
                   <div className="layer inner"></div>
                 </div>
-
               </section>
             </section>
           </section>
-
-
 
           {/* tablet mage section */}
 
           <section className=" hidden max-w-md md:block">
             <section className=" flex justify-center flex-col  ">
-
               <img
                 src={"/Earth.png"}
                 alt="earth"
@@ -149,12 +143,10 @@ export default function HeroSection() {
             </section>
           </section>
 
-
           {/* mobile image section */}
 
           <section className="block md:hidden">
             <section className=" flex justify-center flex-col  ">
-
               <img
                 src={"/Earth.png"}
                 alt="earth"
