6bda2f69fbc2a87a1ae9be34d9a1ba491b2e7f69
Akpan Joseph <syntaxdesigners@gmail.com>
2025-06-28 14:08:53 +0100

    fix: improve styling and hover effects in AboutCard component for enhanced visual appeal

--- src/app/Component/HomePage/AboutCard.tsx
+++ src/app/Component/HomePage/AboutCard.tsx
@@ -13,27 +13,36 @@ interface AboutCardProps {
 
 const AboutCard = ({ title, description, icon, link }: AboutCardProps) => {
     return (
-        <article className="rounded-xl p-8 border border-[#2A2A2A]" style={{ background: 'linear-gradient(rgba(23, 22, 21, 80), rgba(51, 51, 51, 0.20))', }}>
-            <div className="mb-4">
-                {typeof icon === 'string' ? (
-                    <Image src={icon} alt={title} width={30} height={30} />
-                ) : (
-                    icon
-                )}
-            </div>
-            <h3 className="font-semibold text-white text-lg mb-2">{title}</h3>
-            <p className="text-[#EAF5F4A6] text-sm mb-6 leading-relaxed ">
-                {description}
-            </p>
-            {link && (
-                <Link href={link} className="text-white font-semibold text-sm flex items-center gap-1 justify-start hover:text-[#B7F2B0] transition-colors duration-300">
-                    <p>Learn more </p>
-                    <span><FaArrowRight className="text-[#fff]" /></span>
-                </Link>
-            )}
-
-           
-        </article>
+      <article
+        className="rounded-xl p-8 border border-[#2A2A2A]"
+        style={{
+          background:
+            "linear-gradient(rgba(23, 22, 21, 80), rgba(51, 51, 51, 0.20))",
+        }}
+      >
+        <div className="mb-4">
+          {typeof icon === "string" ? (
+            <Image src={icon} alt={title} width={30} height={30} />
+          ) : (
+            icon
+          )}
+        </div>
+        <h3 className="font-semibold text-white text-lg mb-2">{title}</h3>
+        <p className="text-[#EAF5F4A6] text-sm mb-6 leading-relaxed ">
+          {description}
+        </p>
+        {link && (
+          <Link
+            href={link}
+            className="group text-white font-semibold text-sm flex items-center gap-1 justify-start hover:text-[#48B96B] transition-colors duration-100"
+          >
+            <p>Learn more </p>
+            <span>
+              <FaArrowRight className="text-[#fff] transition-transform duration-100 group-hover:text-[#48B96B]" />
+            </span>
+          </Link>
+        )}
+      </article>
     );
 };
 
