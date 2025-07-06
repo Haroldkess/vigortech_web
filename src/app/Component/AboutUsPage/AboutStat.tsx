f4c0cf01b30f8143dc38f551fc70418b48d142e0
akpan josep <syntaxdesigners@gmail.com>
2025-07-06 15:01:05 +0100

    fix: improve layout and styling in AboutStats component for better visual consistency and readability

--- src/app/Component/AboutUsPage/AboutStat.tsx
+++ src/app/Component/AboutUsPage/AboutStat.tsx
@@ -11,27 +11,27 @@ const AboutStats = () => {
   });
 
   return (
-    <section className="md:max-w-7xl w-full px-4 md:mx-auto  md:px-6  flex flex-col  items-center md:flex-row md:justify-center md:items-center gap-12 md:gap-0">
+    <section className=" w-full px-4 md:mx-auto  md:px-6  flex flex-col  items-center md:flex-row md:justify-center md:items-center gap-12 md:gap-0">
       <div
-        className="grid md:grid-cols-4 grid-cols-2  md:gap-x-40 justify-center items-center gap-y-8 md:max-w-md gap-x-20"
+        className="grid md:grid-cols-4 grid-cols-2  md:gap-x-40 justify-center items-center gap-y-8 md:max-w-2xl gap-x-20"
         ref={ref}
       >
         <div className="md:w-[4vw] w-[12vw]">
-          <h3 className="text-4xl font-semiborder border-b border-gray-600 pb-1">
+          <h3 className="text-3xl md:text-5xl font-semibold border-b border-gray-600 pb-1">
             {inView ? <CountUp start={0} end={5} duration={4} /> : "0"}+
           </h3>
           <p className="text-[#EAF5F4A6] mt-1 text-sm">Projects</p>
         </div>
-        <div className="md:w-[8vw] w-[26vw]">
-          <h3 className="text-4xl font-semiborder border-b border-gray-600 pb-1">
+        <div className="md:w-[8vw] ">
+          <h3 className="text-3xl md:text-5xl font-semibold border-b border-gray-600 pb-1">
             {inView ? <CountUp start={0} end={200} duration={4} /> : "0"}+
           </h3>
           <p className="text-[#EAF5F4A6] mt-1  md:text-[12px] text-sm">
             Learners trained
           </p>
         </div>
-        <div className="md:w-[7.5vw] w-[25vw]">
-          <h3 className="text-4xl font-semiborder border-b border-gray-600 pb-1">
+        <div className="md:w-[10vw]">
+          <h3 className="text-3xl md:text-5xl font-semibold border-b border-gray-600 pb-1">
             {inView ? (
               <CountUp start={0} end={99.9} duration={4} decimals={1} />
             ) : (
@@ -39,10 +39,15 @@ const AboutStats = () => {
             )}
             %
           </h3>
-          <p className="text-[#EAF5F4A6] mt-1 text-sm">Satisfaction</p>
+          <p className="text-[#EAF5F4A6] mt-1 text-sm hidden md:block">
+            Satisfied Board
+          </p>
+          <p className="text-[#EAF5F4A6] mt-1 text-sm md:hidden ">
+            Satisfaction
+          </p>
         </div>
-        <div className="md:w-[5.4vw] w-[16vw]">
-          <h3 className="text-4xl font-semiborder border-b border-gray-600 pb-1">
+        <div className="md:w-[5.4vw] ">
+          <h3 className="text-3xl md:text-5xl font-semibold border-b border-gray-600 pb-1">
             10+
           </h3>
           <p className="text-[#EAF5F4A6] mt-1 text-sm">Integrations</p>
